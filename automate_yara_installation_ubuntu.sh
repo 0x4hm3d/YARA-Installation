81527ed73863fa9905d9144bc9dfb38017d4b2af
Ahmed ElHabashi <81084776+0x4hm3d@users.noreply.github.com>
2023-03-09 00:53:02 +0300

    Update and rename automate_yara_install_centos.sh to automate_yara_installation_ubuntu.sh

--- automate_yara_installation_ubuntu.sh
+++ automate_yara_installation_ubuntu.sh
@@ -1,10 +1,14 @@
 apt-get update && apt-get install -y automake libtool make gcc pkg-config && apt-get install -y flex bison
 sleep 5
-wget wget https://github.com/VirusTotal/yara/archive/refs/tags/v4.2.3.tar.gz && tar xzvf v4.2.3.tar.gz
+wget https://github.com/VirusTotal/yara/archive/refs/tags/v4.2.3.tar.gz && tar xzvf v4.2.3.tar.gz
+sleep 5
 cd yara-4.2.3.0
 ./bootstrap.sh
 sleep 10
 ./configure --enable-cuckoo --enable-magic --enable-dotnet
+sleep 10
 make
+sleep 10
 make install
+sleep 10
 make check
