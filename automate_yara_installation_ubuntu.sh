0ba73b7afe2406c6e5459adf683f6e0692008aee
Ahmed ElHabashi <81084776+0x4hm3d@users.noreply.github.com>
2023-03-09 01:08:15 +0300

    Update automate_yara_installation_ubuntu.sh

--- automate_yara_installation_ubuntu.sh
+++ automate_yara_installation_ubuntu.sh
@@ -1,8 +1,8 @@
-apt-get update && apt-get install -y automake libtool make gcc pkg-config && apt-get install -y flex bison
+apt-get update && apt-get install -y automake libtool make gcc pkg-config && apt-get install -y flex bison libjansson-dev libmagic-dev
 sleep 5
 wget https://github.com/VirusTotal/yara/archive/refs/tags/v4.2.3.tar.gz && tar xzvf v4.2.3.tar.gz
 sleep 5
-cd yara-4.2.3.0
+cd yara-4.2.3
 ./bootstrap.sh
 sleep 10
 ./configure --enable-cuckoo --enable-magic --enable-dotnet
